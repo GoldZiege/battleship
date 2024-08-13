a853ebd037f7b41679fc0e43f41e4d297a17d8b3
Marc Zyche <Marc.Zyche@gmail.com>
2024-08-13 07:55:44 +0000

    Add create_battlefield function to create game grid

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+battlefield = []
+battlefield_copy = []
+
+def create_battlefield(length):
+    for x in range(length):
+        add_list = []
+        if x == 0:
+            add_list.append(" ")
+            for y in range(length - 1):
+                add_list.append(chr(y + 65))
+        else:
+            add_list.append(x)
+            for z in range(length - 1):
+                add_list.append("#")
+        battlefield.append(add_list)
+
+    for item in battlefield:
+        print(*item)
+
+def set_ship():
+    global battlefield_copy
+    battlefield_copy = battlefield
+
+
+def main():
+    create_battlefield(7)
+
+main()
+print(random.randrange(6) + 1)
