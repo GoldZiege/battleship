5ce98e4fdb3c9ee71c545943d7470a07779cb32f
Marc Zyche <Marc.Zyche@gmail.com>
2024-08-13 08:55:23 +0000

    Add set_ship function that doesnt work yet.

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import random
 
 battlefield = []
 battlefield_copy = []
+game_size = 7
 
 def create_battlefield(length):
     for x in range(length):
@@ -16,16 +17,34 @@ def create_battlefield(length):
                 add_list.append("#")
         battlefield.append(add_list)
 
+    battlefield_copy = battlefield
+
     for item in battlefield:
         print(*item)
 
-def set_ship():
-    global battlefield_copy
-    battlefield_copy = battlefield
+def set_ship(ship_length):
+    ship_setting = True
+    while ship_setting == True:
+        x_coordinate = random.randrange(game_size - 1) + 1
+        y_coordinate = random.randrange(game_size - 1) + 1
+        direction = random.randrange(2)
+        all_valid = True
+        if direction == 0:
+            for x in range(ship_length):
+                if battlefield_copy[y_coordinate][x_coordinate + x] != "#":
+                    all_valid = False
+                    break
+            if all_valid == True:
+                for z in range(ship_length):
+                    battlefield_copy[y_coordinate][x_coordinate + z] = "X"
+                ship_setting = False
+
 
 
 def main():
-    create_battlefield(7)
+    create_battlefield(game_size)
+    set_ship(1)
+    for item in battlefield_copy:
+        print(*item)
 
 main()
-print(random.randrange(6) + 1)
